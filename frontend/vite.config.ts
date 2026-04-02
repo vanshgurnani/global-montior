74840e999a5b56a464f964b3a5ac2a3e68138b0b
vanshgurnani <gurnanivansh57@gmail.com>
2026-04-03 02:55:30 +0530

    proxy added to forntend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname),
     },
   },
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
